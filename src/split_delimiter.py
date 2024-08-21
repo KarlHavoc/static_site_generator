5ccbd86526126890802a7138ba171410ed1b194f
carlhavoc <mkowalsky87@gmail.com>
2024-08-21 14:54:27 -0700

    Fixed a function that wasn't working. Updated tests.

--- src/split_delimiter.py
+++ src/split_delimiter.py
@@ -10,11 +10,17 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     for node in old_nodes:
         if node.text_type != "text":
             new_nodes.append(node)
-        elif delimiter not in node.text:
-            raise Exception("Delimiter not found in TextNode text")
-        else:
-            split_text_list = node.text.split(f"{delimiter}")
-            new_nodes.append(TextNode(split_text_list[0], text_type_text))
-            new_nodes.append(TextNode(split_text_list[1], text_type))
-            new_nodes.append(TextNode(split_text_list[2], text_type_text))
+            continue
+        split_list = []
+        split_text_list = node.text.split(delimiter)
+        if len(split_text_list) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
+        for i in range(len(split_text_list)):
+            if split_text_list[i] == "":
+                continue
+            if i % 2 == 0:
+                split_list.append(TextNode(split_text_list[i], text_type_text))
+            else:
+                split_list.append(TextNode(split_text_list[i], text_type))
+        new_nodes.extend(split_list)
     return new_nodes
--- src/test_split_delimiter.py
+++ src/test_split_delimiter.py
@@ -25,8 +25,8 @@ class TestSplitDelimiter(unittest.TestCase):
         )
 
     def test_split_delimiter_bold(self):
-        node = TextNode("This is text with a *bold* word", text_type_text)
-        new_node = split_nodes_delimiter([node], "*", text_type_bold)
+        node = TextNode("This is text with a **bold** word", text_type_text)
+        new_node = split_nodes_delimiter([node], "**", text_type_bold)
         self.assertEqual(
             new_node,
             [
@@ -36,13 +36,27 @@ class TestSplitDelimiter(unittest.TestCase):
             ],
         )
 
-    def test_split_delimiter_italic(self):
-        node = TextNode("This is text with a **italic** word", text_type_text)
-        new_node = split_nodes_delimiter([node], "**", text_type_italic)
+    def test_split_delimiter_double_bold(self):
+        node = TextNode("This is **text** with a **bold** word", text_type_text)
+        new_node = split_nodes_delimiter([node], "**", text_type_bold)
         self.assertEqual(
             new_node,
             [
-                TextNode("This is text with a ", text_type_text),
+                TextNode("This is ", text_type_text),
+                TextNode("text", text_type_bold),
+                TextNode(" with a ", text_type_text),
+                TextNode("bold", text_type_bold),
+                TextNode(" word", text_type_text),
+            ],
+        )
+
+    def test_split_delimiter_italic(self):
+        node = TextNode("This is text with an *italic* word", text_type_text)
+        new_node = split_nodes_delimiter([node], "*", text_type_italic)
+        self.assertEqual(
+            new_node,
+            [
+                TextNode("This is text with an ", text_type_text),
                 TextNode("italic", text_type_italic),
                 TextNode(" word", text_type_text),
             ],
