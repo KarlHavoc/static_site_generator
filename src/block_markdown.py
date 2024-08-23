bc6416882c4df6fbdcc1d6bf0fecc34efa615c66
carlhavoc <mkowalsky87@gmail.com>
2024-08-22 17:52:21 -0700

    Add function and test for determining the type of a block of markdown

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,6 @@
+import re
+
+
 def markdown_to_blocks(markdown):
     split_text = markdown.split("\n\n")
     stripped_list = []
@@ -5,3 +8,18 @@ def markdown_to_blocks(markdown):
         stripped_text = text.strip()
         stripped_list.append(stripped_text)
     return stripped_list
+
+
+def block_to_block_type(block):
+    if block[:3] == "```" and block[-3:] == "```":
+        return "code"
+    elif block[:2] == "* " or block[:2] == "- ":
+        return "unordered_list"
+    elif block[:1] == ">":
+        return "quote"
+    elif block[:3] == "1. ":
+        return "ordered_list"
+    elif re.match("#{1,6} ", block) is not None:
+        return "heading"
+    else:
+        return "paragraph"
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import block_to_block_type, markdown_to_blocks
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -26,6 +26,8 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
     def test_markdown_to_blocks_with_whitespace(self):
         md_text_with_whitespace = """    # This is a heading
 
+        
+
     This is a paragraph of text. It has some **bold** and *italic* words inside of it.    
 
  * This is the first list item in a list block
@@ -41,3 +43,33 @@ This is a paragraph of text. It has some **bold** and *italic* words inside of i
                 "* This is the first list item in a list block\n* This is a list item\n* This is another list item",
             ],
         )
+
+    def test_block_to_block_type_(self):
+        paragraph_text = "This is a paragraph"
+        code_text = "```This is a code block```"
+        heading1_text = "# Heading 1"
+        heading2_text = "## Heading 2"
+        heading3_text = "### Heading 3"
+        heading4_text = "#### Heading 4"
+        heading5_text = "##### Heading 5"
+        heading6_text = "###### Heading 6"
+        unordered_list_asterisk = "* Item 1"
+        unordered_list_dash = "- Item 2"
+        quote_text = "> This is a quote"
+        ordered_list_text = "1. Item 1"
+        self.assertEqual(block_to_block_type(paragraph_text), "paragraph")
+        self.assertEqual(block_to_block_type(code_text), "code")
+        self.assertEqual(block_to_block_type(heading1_text), "heading")
+        self.assertEqual(block_to_block_type(heading2_text), "heading")
+        self.assertEqual(block_to_block_type(heading3_text), "heading")
+        self.assertEqual(block_to_block_type(heading4_text), "heading")
+        self.assertEqual(block_to_block_type(heading5_text), "heading")
+        self.assertEqual(block_to_block_type(heading6_text), "heading")
+        self.assertEqual(block_to_block_type(unordered_list_asterisk), "unordered_list")
+        self.assertEqual(block_to_block_type(unordered_list_dash), "unordered_list")
+        self.assertEqual(block_to_block_type(quote_text), "quote")
+        self.assertEqual(block_to_block_type(ordered_list_text), "ordered_list")
+
+
+if __name__ == "__main__":
+    unittest.main()
